f392f147a3c505e21a1cc397441ae26343bf2e39
Jose Copeti <jrcopeti@gmail.com>
2023-11-01 19:18:36 +0100

    key id for movies

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -3,7 +3,7 @@ export default function MovieList({movies, onSelectMovie}) {
   return (
     <ul className="list list-movies">
       {movies?.map((movie)=> (
-        <Movie movie={movie} onSelectMovie={onSelectMovie} />
+        <Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
 
       ))}
 
