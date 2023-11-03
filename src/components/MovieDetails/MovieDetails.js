006b88df5a68806223f2faf15bab8b0e872fb64a
Jose Copeti <jrcopeti@gmail.com>
2023-11-03 16:53:08 +0100

    https correction api

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -65,7 +65,7 @@ export default function MovieDetails({
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
