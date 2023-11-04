bd4dbaf167c784d4eb063fff311eab62e408769d
Jose Copeti <jrcopeti@gmail.com>
2023-11-04 22:58:26 +0100

    star rating size

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -16,6 +16,7 @@ export default function MovieDetails({
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState(0);
   const [error, setError] = useState("");
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
 
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   console.log(isWatched);
@@ -99,6 +100,32 @@ export default function MovieDetails({
     [title]
   );
 
+  useEffect(() => {
+    function handleResize() {
+      setScreenSize(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  function starSize(screenSize) {
+    if (screenSize <= 390) {
+      return 20;
+    } else if (screenSize <= 480) {
+      return 24;
+    } else if (screenSize <= 850) {
+      return 38;
+    } else if (screenSize <= 1285) {
+      return 40;
+    } else {
+      return 45;
+    }
+  }
+
   return (
     <div className="details">
       {isLoading && <Loader />}
@@ -131,10 +158,9 @@ export default function MovieDetails({
               <>
                 <StarRating
                   maxRating={10}
-                  size={24}
+                  size={starSize()}
                   onSetRating={setUserRating}
                   fontFamily="Alata"
-                  className="star-rating"
                 />
                 {!isWatched && userRating >= 0 && (
                   <button className="btn-add" onClick={handleAddMovie}>
